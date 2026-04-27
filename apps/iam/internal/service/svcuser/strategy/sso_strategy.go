646f1353ee79513ab3e14f3c7b32eebdafa5883d
morehao <morehao@qq.com>
2026-04-28 01:47:46 +0800

    feat(iam): 实现SSOStrategy注册策略

--- apps/iam/internal/service/svcuser/strategy/sso_strategy.go
+++ apps/iam/internal/service/svcuser/strategy/sso_strategy.go
@@ -1,24 +1,86 @@
 package strategy
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/morehao/goark/apps/iam/dao"
+	"github.com/morehao/goark/apps/iam/model"
+	"github.com/morehao/goark/pkg/code"
+	"github.com/morehao/golib/glog"
 )
 
 type ssoStrategy struct {
+	*strategyCommon
 }
 
 func NewSSOStrategy() RegisterStrategy {
-	return &ssoStrategy{}
+	return &ssoStrategy{
+		strategyCommon: newStrategyCommon(),
+	}
 }
 
 func (s *ssoStrategy) PreRegister(ctx *gin.Context, req *RegisterRequest) (*RegisterResult, error) {
-	return &RegisterResult{}, nil
+	orgEntity, err := getCurrentOrg(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	ssoType := req.SSOType
+	openID := req.OpenID
+	if openID == "" {
+		return nil, code.GetError(code.AuthRegisterError)
+	}
+
+	tenant, err := s.findTenantBySSO(ctx, orgEntity.ID, ssoType, openID)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.createRegisterResult(ctx, orgEntity.ID, tenant.ID, req, 0)
 }
 
 func (s *ssoStrategy) PostRegister(ctx *gin.Context, req *RegisterRequest, userID uint, result *RegisterResult) error {
+	orgEntity, err := getCurrentOrg(ctx)
+	if err != nil {
+		return err
+	}
+	if err := s.assignDefaultRolesAndDepts(ctx, orgEntity.ID, userID); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (s *ssoStrategy) GetStrategyType() RegisterStrategyType {
 	return RegisterStrategySSO
+}
+
+func (s *ssoStrategy) findTenantBySSO(ctx *gin.Context, orgID uint, ssoType, openID string) (*model.TenantEntity, error) {
+	ssoBind, err := dao.NewSSOBindDao().GetByCond(ctx, &dao.SSOBindCond{
+		OrgID:   orgID,
+		SSOType: ssoType,
+		OpenID:  openID,
+	})
+	if err != nil {
+		glog.Errorf(ctx, "[findTenantBySSO] GetByCond fail, ssoType:%s, err:%v", ssoType, err)
+		return nil, code.GetError(code.AuthRegisterError)
+	}
+	if ssoBind != nil && ssoBind.TenantID > 0 {
+		return dao.NewTenantDao().GetByID(ctx, ssoBind.TenantID)
+	}
+
+	tenantIDStr, err := s.getOrgConfigString(ctx, orgID, model.OrgConfigKeyRegisterSSODefaultTenantID)
+	if err != nil {
+		glog.Errorf(ctx, "[findTenantBySSO] GetString defaultTenantID fail, err:%v", err)
+		return nil, code.GetError(code.AuthRegisterError)
+	}
+	if tenantIDStr == "" {
+		return nil, code.GetError(code.TenantNotExistError)
+	}
+
+	var tenantID uint
+	if _, err := fmt.Sscanf(tenantIDStr, "%d", &tenantID); err != nil {
+		return nil, code.GetError(code.AuthRegisterError)
+	}
+	return dao.NewTenantDao().GetByID(ctx, tenantID)
 }
