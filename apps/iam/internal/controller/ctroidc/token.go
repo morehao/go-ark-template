442abe2d18c51581cd9af6c2625c81fd13d8c956
morehao <morehao@qq.com>
2026-04-27 17:09:13 +0800

    feat(iam): add OIDC controllers

---
+++ apps/iam/internal/controller/ctroidc/token.go
@@ -0,0 +1,66 @@
+package ctroidc
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/morehao/goark/apps/iam/internal/dto/dtooidc"
+	"github.com/morehao/goark/apps/iam/internal/service/svcoidc"
+	"github.com/morehao/golib/biz/gcontext/gincontext"
+)
+
+type tokenCtr struct {
+	tokenSvc svcoidc.TokenSvc
+}
+
+func NewTokenCtr() *tokenCtr {
+	return &tokenCtr{
+		tokenSvc: svcoidc.NewTokenSvc(),
+	}
+}
+
+// Token
+// @Tags OIDC
+// @Summary 获取Token
+// @accept application/json
+// @Produce application/json
+// @Param req formData dtooidc.TokenReq true "Token请求"
+// @Success 200 {object} gincontext.DtoRender{data=dtooidc.TokenResp}
+// @Router /v1/iam/oidc/token [POST]
+func (ctr *tokenCtr) Token(ctx *gin.Context) {
+	var req dtooidc.TokenReq
+	if err := ctx.ShouldBind(&req); err != nil {
+		gincontext.Fail(ctx, err)
+		return
+	}
+
+	resp, err := ctr.tokenSvc.ExchangeCodeForToken(ctx, &req)
+	if err != nil {
+		gincontext.Fail(ctx, err)
+		return
+	}
+
+	gincontext.Success(ctx, resp)
+}
+
+// RefreshToken
+// @Tags OIDC
+// @Summary 刷新Token
+// @accept application/json
+// @Produce application/json
+// @Param req formData dtooidc.TokenRefreshReq true "刷新Token请求"
+// @Success 200 {object} gincontext.DtoRender{data=dtooidc.TokenResp}
+// @Router /v1/iam/oidc/token/refresh [POST]
+func (ctr *tokenCtr) RefreshToken(ctx *gin.Context) {
+	var req dtooidc.TokenRefreshReq
+	if err := ctx.ShouldBind(&req); err != nil {
+		gincontext.Fail(ctx, err)
+		return
+	}
+
+	resp, err := ctr.tokenSvc.RefreshAccessToken(ctx, &req)
+	if err != nil {
+		gincontext.Fail(ctx, err)
+		return
+	}
+
+	gincontext.Success(ctx, resp)
+}
