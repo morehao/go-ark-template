6a8dd58955c9cc90ddda84faa28d233f95bcb228
morehao <morehao@qq.com>
2026-04-28 01:37:24 +0800

    feat(iam): 实现 DomainStrategy 域名驱动注册策略

--- apps/iam/internal/service/svcuser/strategy/domain_strategy.go
+++ apps/iam/internal/service/svcuser/strategy/domain_strategy.go
@@ -1,24 +1,70 @@
 package strategy
 
 import (
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/morehao/goark/apps/iam/dao"
+	"github.com/morehao/goark/apps/iam/model"
+	"github.com/morehao/goark/pkg/code"
+	"github.com/morehao/golib/glog"
 )
 
 type domainStrategy struct {
+	*strategyCommon
 }
 
 func NewDomainStrategy() RegisterStrategy {
-	return &domainStrategy{}
+	return &domainStrategy{
+		strategyCommon: newStrategyCommon(),
+	}
 }
 
 func (s *domainStrategy) PreRegister(ctx *gin.Context, req *RegisterRequest) (*RegisterResult, error) {
-	return &RegisterResult{}, nil
+	orgEntity, err := getCurrentOrg(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	domain := resolveDomainFromHost(ctx)
+	if domain == "" {
+		return nil, code.GetError(code.TenantNotExistError)
+	}
+
+	tenant, err := dao.NewTenantDao().GetByCond(ctx, &dao.TenantCond{
+		OrgID:  orgEntity.ID,
+		Domain: domain,
+	})
+	if err != nil {
+		glog.Errorf(ctx, "[domainStrategy.PreRegister] GetByCond fail, domain:%s, err:%v", domain, err)
+		return nil, code.GetError(code.TenantNotExistError)
+	}
+	if tenant == nil {
+		return nil, code.GetError(code.TenantNotExistError)
+	}
+	if tenant.Status != model.TenantStatusEnabled {
+		return nil, code.GetError(code.AuthRegisterError)
+	}
+
+	return s.createRegisterResult(ctx, orgEntity.ID, tenant.ID, req)
 }
 
 func (s *domainStrategy) PostRegister(ctx *gin.Context, req *RegisterRequest, userID uint) error {
-	return nil
+	orgEntity, err := getCurrentOrg(ctx)
+	if err != nil {
+		return err
+	}
+	return s.assignDefaultRolesAndDepts(ctx, orgEntity.ID, userID)
 }
 
 func (s *domainStrategy) GetStrategyType() RegisterStrategyType {
 	return RegisterStrategyDomain
+}
+
+func resolveDomainFromHost(ctx *gin.Context) string {
+	host := ctx.Request.Host
+	if idx := strings.Index(host, ":"); idx != -1 {
+		host = host[:idx]
+	}
+	return strings.TrimSpace(host)
 }
